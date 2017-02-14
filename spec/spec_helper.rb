05cee6fd724be26338008ad4a866b79b3af42306
Maxim <maksym.kustov@gmail.com>
2017-02-14 23:03:41 +0100

    update rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,4 +14,5 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+  RSpec::Expectations.configuration.on_potential_false_positives = :nothing
 end
